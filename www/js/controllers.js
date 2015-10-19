229722fb7f3464f15400c99072ed0232f52e62bf
yoshimura <yoshimura@srp.co.jp>
2015-10-20 00:48:29 +0900

    init

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,7 +5,7 @@ var onNotification = function(event) {
 };
 
 angular.module('myApp.controllers', [])
-.controller('mainCtrl',function($scope,$cordovaDevice){
+.controller('mainCtrl',function($scope,$cordovaDevice,$http){
 	var _this=this;
 //    document.addEventListener("deviceready", function() {
 //    	_this.model = $cordovaDevice.getModel();
@@ -19,6 +19,24 @@ angular.module('myApp.controllers', [])
     		pushNotification.register(
     				function(token){
     					alert("token:"+token);
+    					
+    					$http({
+    						'method':'POST',
+    						'url':'https://api.parse.com/1/installations',
+    						'headers':{
+    							"X-Parse-Application-Id":"cXj6zBUbKWhDVJtkzpT8Ay5O0bejaHaOE1a3V3lw",
+    							"X-Parse-REST-API-Key": "08dnnfXwTVVkVgeSGJ6gnLFezGBMhe9UY2B9tO9M" 
+    						},
+    						'data':{
+    					        "deviceType": "ios",
+    					        "deviceToken": token,
+    					        "channels": [""]
+    					      }
+    					}).then(function (res) {
+    					    alert(res);
+    						}, function (res) {
+    					    alert(res);
+    					  });
     				},
     				function(err){
     					alert("error:"+err);
