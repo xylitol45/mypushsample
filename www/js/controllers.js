7e13ba31d4ce3629798a68843734da62b89a8998
yoshimura <yoshimura@srp.co.jp>
2015-10-21 00:26:37 +0900

    init

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -21,42 +21,37 @@ angular.module('myApp.controllers', [])
     	if ($cordovaDevice.getPlatform() == 'iOS') {
     		alert('pushNotification');
     		pushNotification.register(
-    				function(token){
-    					alert("token:"+token);
-    					
-    					$http({
-    						'method':'POST',
-    						// 'url': 'http://f-spring.prv11.srp-tech.net/uAhd43rt/20151020/log.php',
-    						'url':'https://api.parse.com/1/installations',
-    						'headers':{
-    							"X-Parse-Application-Id":"cXj6zBUbKWhDVJtkzpT8Ay5O0bejaHaOE1a3V3lw",
-    							"X-Parse-REST-API-Key": "08dnnfXwTVVkVgeSGJ6gnLFezGBMhe9UY2B9tO9M" 
-    						},
-    						'data':{
-    					        "deviceType": "ios",
-    					        "deviceToken": token,
-    					        "channels": ["cat","dog"]
-    					      }
-    					}).then(function (res) {
-    					    alert(res);
-    						}, function (res) {
-    					    alert(res);
-    					  });
-    				},
-    				function(err){
-    					alert("error:"+err);
-    				},
-    				{
-    					"badge":"true",
-    				    "sound":"true",
-    				    "alert":"true",
-    				    "ecb": "onNotificationSample"
-//     				    function(event){
-//     				        if ( event.alert ) {
-//     				        	alert(event.alert);
-//     				        }
-//     				    }
-    				}
+				function(token){
+					alert("token:"+token);
+					
+					$http({
+						'method':'POST',
+						// 'url': 'http://f-spring.prv11.srp-tech.net/uAhd43rt/20151020/log.php',
+						'url':'https://api.parse.com/1/installations',
+						'headers':{
+							"X-Parse-Application-Id":"cXj6zBUbKWhDVJtkzpT8Ay5O0bejaHaOE1a3V3lw",
+							"X-Parse-REST-API-Key": "08dnnfXwTVVkVgeSGJ6gnLFezGBMhe9UY2B9tO9M" 
+						},
+						'data':{
+					        "deviceType": "ios",
+					        "deviceToken": token,
+					        "channels": [""]
+					      }
+					}).then(function (res) {
+					    alert(res);
+						}, function (res) {
+					    alert(res);
+					  });
+				},
+				function(err){
+					alert("error:"+err);
+				},
+				{
+					"badge":"true",
+				    "sound":"true",
+				    "alert":"true",
+				    "ecb": "onNotificationSample"
+ 				}
     		);
     	}
     	_this.model = $cordovaDevice.getPlatform();
