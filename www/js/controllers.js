859301fa951c9fc839b88195e691ac065e4cb280
yoshimura <yoshimura@srp.co.jp>
2015-10-22 00:14:40 +0900

    d

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,12 @@
 "use strict";
 
 angular.module('myApp.controllers', ['ngCordova'])
+
 .controller('mainCtrl',['$scope','$cordovaDevice',function($scope,$cordovaDevice){
 	var _this=this;
 	ionic.Platform.ready(function(){
     	_this.model = $cordovaDevice.getPlatform();
     	$scope.$apply();
-	}]);
-})
+	});
+}])
 ;
