c095bccf6bc4c5a5cccbf117b9f1b2e83bd6aec1
yoshimura <yoshimura@srp.co.jp>
2015-10-21 23:35:07 +0900

    push

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,62 @@
 "use strict";
 
-angular.module('myApp', ['ionic','ngCordova','myApp.controllers'])
+angular.module('myApp', ['ionic','ngCordova','myApp.services','myApp.controllers'])
 
-.run(function($ionicPlatform, $cordovaStatusbar) {	
-	// どうもうまく動作しない
-//  $ionicPlatform.ready(function() {
-//  });
-
-//	document.addEventListener("deviceready", function() {
-//      $cordovaStatusbar.overlaysWebView(true);
-//   }, false);
-
+.run(function($cordovaDevice, $cordovaStatusbar) {	
 	ionic.Platform.ready(function(){
 	    $cordovaStatusbar.overlaysWebView(true);
+
+	    // トークン取得、Parse.com登録
+	    myNotify.registerForiOS();
 	 });
 })
 ;
 
+// 通知受信
+var onMyNotificationAPN = function(event) {
+    if ( event.alert ) {
+    	alert("my notification " + event.alert);
+    }
+};
+
+angular.module('myApp.services', ['ngCordova'])
+.factory('myNotify',function($cordovaDevice,$http){
+	var _o = {
+		// for iOS
+    	registerForiOS: function() {
+    		var pushNotification = window.plugins.pushNotification;
+    		alert('pushNotification');
+    		pushNotification.register(
+				function(token){
+					alert("token:"+token);
+					// Parse.com へのトークン登録
+					$http({
+						'method':'POST',
+						'url':'https://api.parse.com/1/installations',
+						'headers':{
+							"X-Parse-Application-Id":"cXj6zBUbKWhDVJtkzpT8Ay5O0bejaHaOE1a3V3lw",
+							"X-Parse-REST-API-Key": "08dnnfXwTVVkVgeSGJ6gnLFezGBMhe9UY2B9tO9M" 
+						},
+						'data':{
+					        "deviceType": "ios",
+					        "deviceToken": token,
+					        "channels": [""]
+					      }
+					});
+				},
+				function(err){
+					// トークン取得失敗
+					alert("error:"+err);
+				},
+				{
+					"badge":"true",
+				    "sound":"true",
+				    "alert":"true",
+				    "ecb": "onMyNotificationAPN"
+ 				}
+    		);
+    	}
+	};
+	return _o;
+})
+;
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,58 +1,11 @@
 "use strict";
 
-var onMyNotificationAPN = function(event) {
-    if ( event.alert ) {
-    	alert("my notification " + event.alert);
-    }
-};
-
 angular.module('myApp.controllers', [])
 .controller('mainCtrl',function($scope,$cordovaDevice,$http){
 	var _this=this;
-//    document.addEventListener("deviceready", function() {
-//    	_this.model = $cordovaDevice.getModel();
-//    	$scope.$apply();
-//      }, false);
 	ionic.Platform.ready(function(){
-    	var pushNotification = window.plugins.pushNotification;
-    	if ($cordovaDevice.getPlatform() == 'iOS') {
-    		alert('pushNotification');
-    		pushNotification.register(
-				function(token){
-					alert("token:"+token);
-					$http({
-						'method':'POST',
-						'url':'https://api.parse.com/1/installations',
-						'headers':{
-							"X-Parse-Application-Id":"cXj6zBUbKWhDVJtkzpT8Ay5O0bejaHaOE1a3V3lw",
-							"X-Parse-REST-API-Key": "08dnnfXwTVVkVgeSGJ6gnLFezGBMhe9UY2B9tO9M" 
-						},
-						'data':{
-					        "deviceType": "ios",
-					        "deviceToken": token,
-					        "channels": [""]
-					      }
-					}).then(function (res) {
-					    alert(res);
-						}, function (res) {
-					    alert(res);
-					  });
-				},
-				function(err){
-					alert("error:"+err);
-				},
-				{
-					"badge":"true",
-				    "sound":"true",
-				    "alert":"true",
-				    "ecb": "onMyNotificationAPN"
- 				}
-    		);
-    	}
     	_this.model = $cordovaDevice.getPlatform();
     	$scope.$apply();
-
-	 });
-
+	});
 })
 ;
