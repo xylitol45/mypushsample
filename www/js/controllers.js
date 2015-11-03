fafe5b33205834d672dd3201a1dad9c65a953741
yoshimura <yoshimura@srp.co.jp>
2015-11-03 18:06:50 +0900

    d

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,8 +2,11 @@
 
 angular.module('myApp.controllers', ['ngCordova'])
 
-.controller('mainCtrl',['$scope','$cordovaDevice',function($scope,$cordovaDevice){
+.controller('mainCtrl',['$scope','$cordovaDevice','shared',function($scope,$cordovaDevice,shared){
 	var _this=this;
+	_this.onRight = function(){
+		alert('hello');
+	};
 	ionic.Platform.ready(function(){
     	_this.model = $cordovaDevice.getPlatform();
     	$scope.$apply();
